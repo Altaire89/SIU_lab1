7ce5ab52c90e08a3d22f88a40591bbc80365919b
Altaire89 <124275197+Altaire89@users.noreply.github.com>
2024-02-16 14:08:26 +0100

    Configurar TouchEvent

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 let coordinates = [182, 342];
+let marker;
+let destiny;
+let mymap; // Declaramos mymap fuera de la función createMap
 
 function createMap() {
-    const mymap = L.map('sample_map').setView(coordinates, 15);
+    mymap = L.map('sample_map').setView(coordinates, 15);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
         maxZoom: 18
     }).addTo(mymap);
 
-    mymap.on('click', function(e) {
-        console.log(e);
-        let destiny = e.coordinates;
+    marker = L.marker(coordinates).addTo(mymap);
+
+    if (isMobileDevice()) {
+        document.getElementById('sample_map').addEventListener('touchend', function(e) {
+        if (e.touches.length > 0) { // Verificar si hay algún toque registrado
+            const touch = e.touches[0]; // Obtener el primer toque
+            const clickedCoordinates = mymap.containerPointToLatLng(L.point(touch.clientX, touch.clientY));
+            addMarker([clickedCoordinates.lat, clickedCoordinates.lng]);
+        }
     });
-    L.marker([40.3, -3.8]).addTo(mymap);
+    } else{
+        mymap.on('click', function(e) {
+            const clickedCoordinates = [e.latlng.lat, e.latlng.lng];
+            addMarker(clickedCoordinates);
+        });
+    }
 }
 
 if ('geolocation' in navigator) {
@@ -29,4 +47,12 @@ if ('geolocation' in navigator) {
 } else {
     console.error('Geolocation is not available.');
     createMap();
+}
+
+function addMarker(coords) {
+    if (destiny) {
+        destiny.setLatLng(coords); // Actualizar posición del marcador
+    } else {
+        destiny = L.marker(coords).addTo(mymap); // Crear nuevo marcador si no existe
+    }
 }
